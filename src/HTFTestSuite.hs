4018ad5506296bb3ae5477275fda7f2e4bb3b0f2
Nikita Volkov <nikita.y.volkov@mail.ru>
2014-04-01 05:40:51 +0400

    Basic tests

--- src/HTFTestSuite.hs
+++ src/HTFTestSuite.hs
@@ -3,6 +3,7 @@
 import Test.Framework
 import TypeStructure.Prelude.Basic
 
+import {-@ HTF_TESTS @-} TypeStructureTest
 import {-@ HTF_TESTS @-} TypeStructure.THTest
 
 main = htfMain $ htf_thisModulesTests : htf_importedTests
---
+++ src/TypeStructureTest.hs
@@ -0,0 +1,17 @@
+{-# OPTIONS_GHC -F -pgmF htfpp #-}
+module TypeStructureTest where
+
+import Test.Framework
+import TypeStructure.Prelude.Basic
+import TypeStructure.Prelude.Data
+import TypeStructure
+
+
+test_differentStructuresDontEqual = do
+  assertNotEqual (graph (undefined :: Int16)) (graph (undefined :: Int8))
+
+test_sameStructuresDoEqual = do
+  assertEqual (graph (undefined :: Int)) (graph (undefined :: Int))
+
+-- test_differentStructuresProduceDifferentHashes = do
+--   assertNotEqual (hash $ graph (undefined :: Int16)) (hash $ graph (undefined :: Int8))
